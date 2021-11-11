8329fcca99467f9f42ad62ca00a49c2b78f425fa
a <b>
2021-11-11 17:17:41 +0800

    finish sleep lab

---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        fprintf(2, "usage: sleep [seconds]\n");
+        exit(1);
+    }
+    int time = atoi(argv[1]);
+    sleep(time);
+
+    exit(0);
+}
